33654f56b1acb5494f39c61aa8dd15912e748c2f
Jon Mico <jonmico@gmail.com>
2023-03-12 23:03:41 -0400

    initial createNewFood routing, test saving to database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const Food = require("./models/food");
 
 app.set("view engine", "ejs");
 
@@ -14,6 +15,20 @@ const connectAsync = async () => {
 };
 
 connectAsync();
+
+app.get("/", (req, res) => {
+  res.send("this is the home page");
+});
+
+app.get("/createNewFood", async (req, res) => {
+  // const food = new Food({
+  //   brand: "Kroger",
+  //   name: "Spaghetti",
+  //   serving: 56,
+  // });
+  // await food.save();
+  // res.send(food);
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
